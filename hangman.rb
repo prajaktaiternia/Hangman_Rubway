780b964f29ba642f0a59f7171cd7e811bdfe8a3b
prajaktaa <prajakta@iternia.com>
2013-08-22 19:41:25 +0530

    Hangman_rubyway

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,55 @@
 class Hangman
-
- attr_reader :counter
- attr_reader :word_placeholder
- attr_reader :missed_counter
- attr_reader :wrong_choices
+  attr_reader :counter, :word_placeholder, :missed_counter, :repeated_choices #, :wrong_choices
+  attr_accessor :guess_input
 
   def initialize(word)
-     @counter=0
-     @word_placeholder=['_']*word.length
-    @missed_counter=0
-    @wrong_choices=[]
+    @word = word
+    @counter = 0
+    @word_placeholder = ['_']*word.length
+    @missed_counter = 0
+    #@wrong_choices = []
+    @repeated_choices = []
+  end
 
+  def user_input
+  end
+
+  def play_game
+    if @repeated_choices.index(@guess_input).nil? && !(@guess_input.match(/^[[:alpha:]]+$/).nil?)
+      @repeated_choices.push(@guess_input)
+
+      if @guess_input.length > 1
+        if @word == @guess_input
+          @counter = @word.length
+          result
+        else
+          @missed_counter += 1
+          @repeated_choices.push(@guess_input)
+        end
+      else
+        wordString = @word.split("")
+        position = wordString.each_index.select{|i| wordString[i].casecmp(@guess_input) == 0}
+        if position.length > 0
+          @counter += position.length
+          position.each do |index|
+            word_placeholder[index]= @guess_input
+          end
+          puts word_placeholder
+        else
+          @missed_counter += 1
+        end
+      end
+    else
+      puts "You already entered that before"
+    end
+  end
+
+  def result
+    if @counter == @word.length
+      puts "Win"
+    end
+    if @missed_counter == 6
+      puts "Loss"
+    end
   end
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -1,102 +1,103 @@
 require './hangman'
 
 describe Hangman do
-     subject(:hangman){Hangman.new('elephantisis')}
-
-
-     context "input_character" do
-
-
-
-       context "alphabet_check" do
-
-         it "it should accept alphabet only" do
-           hangman.guess_input='ee'
-           expect(hangman.guess_input.match(/^[[:alpha:]]+$/).nil? ).to eq(false)
-         end
-       end
-
-       context "input_one" do
-
-          it "should accept only one character from user" do
-            hangman.guess_input='a'
-            expect(hangman.guess_input.length).to eq(1)
-
-          end
-        end
-
-       context "input_multiple" do
-
-         it "should accept multiple characters from user" do
-           hangman.guess_input='elephantisis'
-           expect(hangman.guess_input).to eq('word')
-         end
-       end
-
-     end
+  subject(:hangman) { Hangman.new('elephantisis') }
 
   context "#initialize" do
-      it "counter should be zero" do
-        expect(hangman.counter).to eq(0)
-      end
-
-      it "counter should not be writable" do
-        expect {hangman.counter=10}.to raise_error NoMethodError
-      end
-
-      it "word_placeholder should be array of '_' " do
-         expect(hangman.word_placeholder).to eq(['_']*12)
-      end
-
-      it "word_placeholder length should be length of the word"  do
-        expect(hangman.word_placeholder.length).to eq(12)
-      end
-
-      it "missed_counter should be zero" do
-        expect(hangman.missed_counter).to eq(0)
-      end
-
-
-      it " wrong_choices should be empty array " do
-        expect(hangman.wrong_choices).to eq([])
-      end
-
+    it "counter should be zero" do
+      expect(hangman.counter).to eq (0)
+    end
+    it "counter should not be writable" do
+      expect{hangman.counter = 10}.to raise_error NoMethodError
+    end
+    it "word_placeholder should be array of '_'" do
+      expect(hangman.word_placeholder).to eq (['_']*12)
+    end
+    it "word_placeholder length should be of the word" do
+      expect(hangman.word_placeholder.length).to eq(12)
+    end
+    it "missed_counter should be 0" do
+      expect(hangman.missed_counter).to eq(0)
+    end
+    it "missed_counter should not be writable" do
+      expect{hangman.missed_counter = 10}.to raise_error NoMethodError
+    end
+    it "repeated_choices should be empty array" do
+      expect(hangman.repeated_choices).to eq([])
+    end
   end
 
+  context "user_input" do
+    it "user input should accept only alphabet" do
+      hangman.guess_input = 'q'
+      #expect(hangman.guess_input).match(/^[[:alpha:]]$/)
+      expect(hangman.guess_input.match(/^[[:alpha:]]+$/).nil? ).to eq(false)
+    end
 
-     context "playgame" do
-       context  "correct_choice" do
-         it "should be check previous counter and increment counter" do
-                expect(hangman.counter).should=+1
-         end
-       end
+    it "user input should accept only 1 character" do
+      hangman.guess_input = 'a'
+      expect(hangman.guess_input.length).to eq(1)
+    end
 
-       context "incorrect_choice" do
-         it "should be increment missed_counter" do
-              expect(hangman.missed_counter).should=+1
-         end
-       end
+    it "user input should accept multiple characters" do
+      hangman.guess_input = 'word'
+      expect(hangman.guess_input).to eq('word')
+    end
+  end
 
-       context "repeated_characters" do
-         it "should be check entered existing character" do
-            expect(hangman.repeated_choices).include?('guess_input')
-           end
-         end
-     end
+  context "#play_game" do
+    context "correct_choice" do
+      it"input character should be included in word" do
+        # position = 2
+        hangman.guess_input = 'e'
+        hangman.play_game
+        hangman.word_placeholder.index('e').should_not == nil
+        #expect(hangman.word_placeholder[position] = guess_input)
+      end
+      it "counter should get incremented by no. of times characters present" do
+        hangman.guess_input = 'e'
+        temp = hangman.counter
+        hangman.play_game
+        (hangman.counter - temp).should == 2
+      end
+    end
 
+    context "for wrong choice" do
+      it"input character should not be included in word" do
+        hangman.guess_input = 'z'
+        expect(hangman.word_placeholder.index('z')).to eq (nil)
+      end
+      it "missed counter should get incremented by 1" do
+        hangman.guess_input = 'q'
+        temp = hangman.missed_counter
+        hangman.play_game
+        (hangman.missed_counter - temp).should == 1
+      end
+    end
+
+    context "for repeated characters" do
+      it"check if user entered already existing characters" do
+        #expect(hangman.repeated_choices).include?('guess_input')
+        hangman.guess_input = 'i'
+        temp = hangman.counter
+        hangman.play_game
+        (hangman.counter - temp).should == 2
+      end
+    end
+  end
 
   context "result" do
     context "win" do
-       it "should be counter 12" do
-         expect(hangman.counter).to eq(12)
-       end
-    end
-
-    context "lose" do
-      it "should be counter 6" do
-        expect(hangman.counter).to eq(6)
+      it "counter should be 12" do
+        expect(hangman.counter).to eq (12)
       end
     end
 
+    context "lose" do
+      it "missed_counter should be 6" do
+        expect(hangman.missed_counter).to eq(6)
+      end
+    end
   end
+
 end
