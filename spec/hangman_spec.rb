9321edec2323988d11e743a91b8ce2d92c4ab10b
prajaktaa <prajakta@iternia.com>
2013-08-22 17:01:25 +0530

    Hangman_rubyway

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -3,8 +3,37 @@ require './hangman'
 describe Hangman do
      subject(:hangman){Hangman.new('elephantisis')}
 
-     it "should accept only one alphabet from user"
-     it "it should accept one alphabet only"
+
+     context "input_character" do
+
+
+
+       context "alphabet_check" do
+
+         it "it should accept alphabet only" do
+           hangman.guess_input='ee'
+           expect(hangman.guess_input.match(/^[[:alpha:]]+$/).nil? ).to eq(false)
+         end
+       end
+
+       context "input_one" do
+
+          it "should accept only one character from user" do
+            hangman.guess_input='a'
+            expect(hangman.guess_input.length).to eq(1)
+
+          end
+        end
+
+       context "input_multiple" do
+
+         it "should accept multiple characters from user" do
+           hangman.guess_input='elephantisis'
+           expect(hangman.guess_input).to eq('word')
+         end
+       end
+
+     end
 
   context "#initialize" do
       it "counter should be zero" do
@@ -34,25 +63,39 @@ describe Hangman do
 
   end
 
-  context  "hangman for correct choice" do
 
-  end
+     context "playgame" do
+       context  "correct_choice" do
+         it "should be check previous counter and increment counter" do
+                expect(hangman.counter).should=+1
+         end
+       end
 
-  context "hangman for incorrect choice" do
+       context "incorrect_choice" do
+         it "should be increment missed_counter" do
+              expect(hangman.missed_counter).should=+1
+         end
+       end
 
-  end
+       context "repeated_characters" do
+         it "should be check entered existing character" do
+            expect(hangman.repeated_choices).include?('guess_input')
+           end
+         end
+     end
 
-  context "check for repeated characters" do
-
-  end
 
   context "result" do
     context "win" do
-
+       it "should be counter 12" do
+         expect(hangman.counter).to eq(12)
+       end
     end
 
     context "lose" do
-
+      it "should be counter 6" do
+        expect(hangman.counter).to eq(6)
+      end
     end
 
   end
